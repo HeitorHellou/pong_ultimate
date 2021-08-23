129a8255df47e2aec1ae894d4452a227a5aa80af
Heitor Hellou Marcondes <heitor.hellou@outlook.com>
2021-08-23 10:43:37 -0300

    Ball physics

--- Pong Ultimate/Assets/Scripts/BallController.cs
+++ Pong Ultimate/Assets/Scripts/BallController.cs
@@ -8,10 +8,17 @@ public class BallController : MonoBehaviour
 
   Rigidbody rb;
 
+  private Vector3 lastFrameVelocity;
+
   private void Start()
   {
     rb = GetComponent<Rigidbody>(); // Rigid body link
     StartCoroutine("WaitLaunch"); // Launching the ball
+  }
+
+  private void FixedUpdate()
+  {
+    lastFrameVelocity = rb.velocity;
   }
 
   public void LaunchBall()
@@ -19,8 +26,8 @@ public class BallController : MonoBehaviour
     float dx = Random.Range(0, 2) == 0 ? -1 : 1; // Random x value to determine which side the ball will go
     float dy = Random.Range(0, 2) == 0 ? -1 : 1; // Where the ball will launch in the Y axis
 
-    //rb.velocity = new Vector3(dx, dy, 0.0f) * speed; // Launching the ball
-    rb.velocity = new Vector3(-1, 0.0f, 0.0f) * speed; // TEST
+    rb.velocity = new Vector3(dx, dy, 0.0f) * speed; // Launching the ball
+    //rb.velocity = new Vector3(-1, 0.0f, 0.0f) * speed; // TEST
   }
 
   private void OnTriggerEnter(Collider other)
@@ -62,33 +69,49 @@ public class BallController : MonoBehaviour
   {
     if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player 2"))
     {
-      ChangeBallBounce(Vector3.Reflect(rb.velocity.normalized, collision.contacts[0].normal), collision.GetContact(0).point.y - collision.transform.position.y);
+      //ChangeBallBounce(Vector3.Reflect(rb.velocity.normalized, collision.contacts[0].normal), collision.GetContact(0).point.y - collision.transform.position.y);
+      ChangeBallBounce(collision.contacts[0].normal, collision.GetContact(0).point.y - collision.transform.position.y);
     }
+
+    CollisionDetected(collision.contacts[0].normal);
+  }
+
+  public Vector3 CollisionDetected(Vector3 collisionNormal)
+  {
+    return Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
   }
 
   // Boucing the ball back relative to distance from paddle center
-  public void ChangeBallBounce(Vector3 direction, float y)
+  public void ChangeBallBounce(Vector3 collisionNormal, float y)
   {
+    var ballVelocity = lastFrameVelocity.magnitude;
+    var bounceDirection = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+    Vector3 direction;
+
     if (y > 0.1)
     {
       if (y < 0.483)
-        rb.AddForce(new Vector3(direction.x * -1, direction.y + 75f, direction.z) * speed * Time.deltaTime);
+        direction = Vector3.Lerp(bounceDirection, new Vector3(bounceDirection.x, bounceDirection.y + 0.5f, bounceDirection.z), 0.3f);
       else if (y < 0.866)
-        rb.AddForce(new Vector3(direction.x * -1, direction.y + 150f, direction.z) * speed * Time.deltaTime);
+        direction = Vector3.Lerp(bounceDirection, new Vector3(bounceDirection.x, bounceDirection.y + 1f, bounceDirection.z), 0.3f);
       else
-        rb.AddForce(new Vector3(direction.x * -1, direction.y + 225f, direction.z) * speed * Time.deltaTime);
+        direction = Vector3.Lerp(bounceDirection, new Vector3(bounceDirection.x, bounceDirection.y + 2f, bounceDirection.z), 0.3f);
     }
     else if (y < -0.1)
     {
       if (y > -0.483)
-        rb.AddForce(new Vector3(direction.x * -1, direction.y + -75f, direction.z) * speed * Time.deltaTime);
+        direction = Vector3.Lerp(bounceDirection, new Vector3(bounceDirection.x, bounceDirection.y -0.5f, bounceDirection.z), 0.3f);
       else if (y > -0.866)
-        rb.AddForce(new Vector3(direction.x * -1, direction.y + - 150f, direction.z) * speed * Time.deltaTime);
+        direction = Vector3.Lerp(bounceDirection, new Vector3(bounceDirection.x, bounceDirection.y -1f, bounceDirection.z), 0.3f);
       else
-        rb.AddForce(new Vector3(direction.x * -1, direction.y - 225f, direction.z) * speed * Time.deltaTime);
+        direction = Vector3.Lerp(bounceDirection, new Vector3(bounceDirection.x, bounceDirection.y -2f, bounceDirection.z), 0.3f);
     }
     else
-      rb.AddForce(new Vector3(direction.x * -1, 0.0f, direction.z) * speed * Time.deltaTime);
+      direction = Vector3.Lerp(bounceDirection, new Vector3(bounceDirection.x, 0.0f, bounceDirection.z), 0.3f);
+
+    Debug.Log("bounce direction: " + direction);
+
+    rb.velocity = direction * speed;
   }
 
   // Delay for ball launch
